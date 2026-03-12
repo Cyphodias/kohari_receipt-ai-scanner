d9ff5510439bab3ca3ee59e4186f04f2f9f9daed
Cyphodias <vinceohola@gmail.com>
2026-03-12 23:00:59 +0300

    Add OCR function using pytesseract and OpenCV

--- backend/app/ocr.py
+++ backend/app/ocr.py
@@ -1,1 +1,12 @@
+import pytesseract
+import cv2
 
+def run_ocr(path):
+
+    image = cv2.imread(path)
+
+    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+
+    text = pytesseract.image_to_string(gray)
+
+    return text
